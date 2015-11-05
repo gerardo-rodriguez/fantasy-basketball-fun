13355b460b10b3b4f6553d15b2509c78e04746b5
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2015-11-05 10:45:45 -0800

    Add grant, setup config

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// Add OAuth modules
+var session = require('express-session');
+var Grant = require('grant-express');
+var grant = new Grant(require('./config.json'));
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,11 +25,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// Required
+app.use(session({
+  secret: 'something something secret',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(grant);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
-app.use(oauth);
+app.use('/connect/yahoo/callback', oauth);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/oauth.js
+++ routes/oauth.js
@@ -1,29 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
-// TODO: Put the consumerKey and consumerSecret in a safer place!!!
-passport.use(new YahooStrategy({
-    consumerKey: 'dj0yJmk9VTYxNlFEd014bnpDJmQ9WVdrOVNtZEtjRFZzTm04bWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0xNw--',
-    consumerSecret: 'c26c3cda6c3894d67d65bbe86292e24885073971',
-    callbackURL: 'http://fantasy-basketball-fun.herokuapp.com/auth/yahoo/callback'
-  },
-  function(token, tokenSecret, profile, done) {
-    User.findOrCreate({ yahooId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
-  }
-));
-
-router.get('/auth/yahoo', passport.authenticate('yahoo'));
-
-router.get('/auth/yahoo/callback',
-  passport.authenticate(
-    'yahoo', {
-      successRedirect: '/',
-      failureRedirect: '/login'
-    }
-  )
-);
+router.get('/', function (req, res, next) {
+  console.log(req.query);
+  res.end(JSON.stringify(req.query, null, 2));
+})
 
 module.exports = router;
