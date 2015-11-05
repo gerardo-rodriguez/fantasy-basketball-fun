35d196a1e4df500dfd8dbde29789d78e861cff0d
Gerardo Rodriguez <ger.rod34@gmail.com>
2015-11-04 20:57:35 -0800

    Add options to stop warnings

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ secret: 'keyboard cat' }));
+app.use(session({ 
+  secret: 'secret sauce',
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
