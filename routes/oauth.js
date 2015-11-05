bc2644ef916016b00f548c5c89ba35f15bce8dfd
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2015-11-05 12:46:22 -0800

    Add log

--- routes/oauth.js
+++ routes/oauth.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  console.log(req.query);
+  console.log('request query:', req.query);
   // res.end(JSON.stringify(req.query, null, 2));
   res.redirect('/');
 })
