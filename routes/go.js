fd9b489009670f6c94b2e7e66aaccdf9eda8ccb4
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2015-11-06 11:33:20 -0800

    Test new route

--- routes/go.js
+++ routes/go.js
@@ -10,19 +10,20 @@ router.get('/', function(req, res, next) {
   console.log('session:', req.session);
 
   fantasysports.request(req, res)
-    .api('http://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=nba/leagues?format=json')
+    // .api('http://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=nba/leagues?format=json')
+    .api('http://fantasysports.yahooapis.com/fantasy/v2/league?format=json')
     .done(function(data) {
-      console.log('--------> the data:', data.fantasy_content.users[0]);
-      console.log('--------> the data:', data.fantasy_content.users[0].user[1]);
-      console.log('--------> the data:', data.fantasy_content.users[0].user[1].games[0]);
-      var leagueData = data.fantasy_content.users[0].user[1].games[0].game[1].leagues,
-          leagues = [];
-
-      _.each(leagueData, function(value) {
-        if (value.league) {
-          leagues.push(value.league[0]);
-        }
-      });
+      // console.log('--------> the data:', data.fantasy_content.users[0]);
+      // console.log('--------> the data:', data.fantasy_content.users[0].user[1]);
+      // console.log('--------> the data:', data.fantasy_content.users[0].user[1].games[0]);
+      // var leagueData = data.fantasy_content.users[0].user[1].games[0].game[1].leagues,
+      //     leagues = [];
+      //
+      // _.each(leagueData, function(value) {
+      //   if (value.league) {
+      //     leagues.push(value.league[0]);
+      //   }
+      // });
 
       // res.json(leagues);
       // res.json(JSON.stringify({ a:1, b:2, c:3 }, null, 4));
