589fe995a247ab56731c47b21b2961f5d52a8152
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2015-11-06 11:08:22 -0800

    Fix lookup in log

--- routes/go.js
+++ routes/go.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
   fantasysports.request(req, res)
     .api('http://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=nba/leagues?format=json')
     .done(function(data) {
-      console.info('--------> the data:', data.fantasy_content.users[0].games[0]);
+      console.info('--------> the data:', data.fantasy_content.users[0].user[1].games[0]);
       var leagueData = data.fantasy_content.users[0].user[1].games[0].game[1].leagues,
           leagues = [];
 
