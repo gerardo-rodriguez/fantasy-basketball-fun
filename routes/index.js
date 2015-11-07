2b98b789912498b30940120ae113c9ede9a0c954
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2015-11-06 16:13:54 -0800

    Add redirect

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,11 @@ router.get('/', function(req, res, next) {
   // var leagues = data['fantasy_content']['users'][0]['user'][1]['games'][0]['game'][1]['leagues'][0]['league'];
   // res.render('index', { title: 'Express', leagues: leagues });
   console.log(req.session);
-  res.render('index', { title: 'Express' });
+  if (req.session.oauthAccessToken !== undefined) {
+    res.redirect('/leagues');
+  } else {
+    res.render('index', { title: 'Express' });
+  }
 });
 
 module.exports = router;
