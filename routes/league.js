848acb1151325799af18d3093eb4c8e1dbe4c658
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2015-11-09 11:47:29 -0800

    Create players route

--- routes/league.js
+++ routes/league.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fantasysports = require('fantasysports');
 var router = express.Router();
 
-/* GET home page. */
+/* GET league page. */
 router.get('/:id', function(req, res, next) {
   var leagueID = req.params.id;
 
@@ -13,6 +13,20 @@ router.get('/:id', function(req, res, next) {
 
       // res.json(data);
       res.render('league', { title: league.name, league: league });
+    });
+});
+
+/* GET league page. */
+router.get('/:id/players', function(req, res, next) {
+  var leagueID = req.params.id;
+
+  fantasysports.request(req, res)
+    .api('http://fantasysports.yahooapis.com/fantasy/v2/league/' + leagueID + '/players;status=A?format=json')
+    .done(function(data) {
+      // var league = data['fantasy_content'].league[0];
+
+      res.json(data);
+      // res.render('league', { title: league.name, league: league });
     });
 });
 
