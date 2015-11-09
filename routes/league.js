7b1ed68b99e469e360ae65709cd325d76bbfe87f
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2015-11-09 12:04:02 -0800

    Show players

--- routes/league.js
+++ routes/league.js
@@ -23,10 +23,10 @@ router.get('/:id/players', function(req, res, next) {
   fantasysports.request(req, res)
     .api('http://fantasysports.yahooapis.com/fantasy/v2/league/' + leagueID + '/players;status=A?format=json')
     .done(function(data) {
-      // var league = data['fantasy_content'].league[0];
+      var players = data['fantasy_content'].league[1].players;
 
-      res.json(data);
-      // res.render('league', { title: league.name, league: league });
+      // res.json(players);
+      res.render('players', { title: 'Available Players', players: players });
     });
 });
 
