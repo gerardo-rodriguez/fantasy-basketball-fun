88072dd4ac5d311a34304018db0d2ca06e4c2e43
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2015-11-23 15:50:12 -0800

    Remove old code

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
+'use strict';
+
 var express = require('express');
-// var data = require('../league-data.json');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // var leagues = data['fantasy_content']['users'][0]['user'][1]['games'][0]['game'][1]['leagues'][0]['league'];
-  // res.render('index', { title: 'Express', leagues: leagues });
   console.log(req.session);
+
   if (req.session.oauthAccessToken !== undefined) {
     res.redirect('/leagues');
   } else {
