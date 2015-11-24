5115e4716f6db77f66edee15febf5bcf11036894
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2015-11-24 12:50:26 -0800

    Show actual errors

--- views/error.jsx
+++ views/error.jsx
@@ -7,10 +7,9 @@ var ErrorView = React.createClass({
   render: function() {
     return (
       <DefaultLayout title={this.props.title}>
-        <h1>Hi</h1>
-        // <h1>messages: {this.props.message}</h1>
-        // <h2>status: {this.props.error.status}</h2>
-        // <pre>error stack: {this.props.error.stack}</pre>
+        <h1>messages: {this.props.message}</h1>
+        <h2>status: {this.props.error.status}</h2>
+        <pre>error stack: {this.props.error.stack}</pre>
       </DefaultLayout>
     );
   }
